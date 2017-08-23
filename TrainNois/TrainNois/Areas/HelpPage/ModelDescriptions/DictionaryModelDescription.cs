fa344a40df809e24d469bc1b302441e40cb2e2fb
thu vo <thu.vo@newoceaninfosys.com>
2017-08-23 11:38:49 +0700

    create project

---
+++ TrainNois/TrainNois/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TrainNois.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ TrainNois/TrainNois/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TrainNois.WebApiApplication" Language="C#" %>
